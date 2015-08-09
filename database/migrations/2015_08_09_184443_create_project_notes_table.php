275d1c2306d86a7c0ac31422dbce7ca45c2532da
Everton Muniz <munizeverton@gmail.com>
2015-08-09 18:55:29 +0000

    Criando migration projects_notes

---
+++ database/migrations/2015_08_09_184443_create_project_notes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectNotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_notes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('note');
+            $table->timestamps();
+            $table->integer('project_id')->unsigned();
+        });
+
+        Schema::table('project_notes', function($table){
+            $table->foreign('project_id')->references('id')->on('projects');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('project_notes');
+    }
+}
