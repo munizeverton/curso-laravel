90b0b22f07c2beb818d37e147ef7872ad786ecb4
Everton Muniz <munizeverton@gmail.com>
2015-08-20 01:30:05 +0000

    Registrando providers

--- app/Providers/CursoLaravelRepositoryProvider.php
+++ app/Providers/CursoLaravelRepositoryProvider.php
@@ -2,12 +2,15 @@
 
 namespace CursoLaravel\Providers;
 
+use CursoLaravel\Entities\ProjectTask;
 use CursoLaravel\Repositories\ClientRepository;
 use CursoLaravel\Repositories\ClientRepositoryEloquent;
 use CursoLaravel\Repositories\ProjectNoteRepository;
 use CursoLaravel\Repositories\ProjectNoteRepositoryEloquent;
 use CursoLaravel\Repositories\ProjectRepository;
 use CursoLaravel\Repositories\ProjectRepositoryEloquent;
+use CursoLaravel\Repositories\ProjectTaskRepository;
+use CursoLaravel\Repositories\ProjectTaskRepositoryEloquent;
 use Illuminate\Support\ServiceProvider;
 
 class CursoLaravelRepositoryProvider extends ServiceProvider
@@ -42,6 +45,11 @@ class CursoLaravelRepositoryProvider extends ServiceProvider
         $this->app->bind(
             ProjectNoteRepository::class,
             ProjectNoteRepositoryEloquent::class
+        );
+
+        $this->app->bind(
+            ProjectTaskRepository::class,
+            ProjectTaskRepositoryEloquent::class
         );
     }
 }
