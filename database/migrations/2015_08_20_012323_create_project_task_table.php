11a0cbd8e809ffba34cfb0ebf3101b4fdddb15ff
Everton Muniz <munizeverton@gmail.com>
2015-08-20 01:27:03 +0000

    Criando migrations

---
+++ database/migrations/2015_08_20_012323_create_project_task_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectTaskTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_task', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->dateTime('start_date');
+            $table->dateTime('due_date');
+            $table->string('status');
+            $table->timestamps();
+            $table->integer('project_id')->unsigned();
+            $table->foreign('project_id')->references('id')->on('projects');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('project_task');
+    }
+}
